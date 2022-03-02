6caa106861401a8006be8e534e5d1b918e73053f
Luis <luyimiranda07@gmail.com>
2022-03-01 21:07:14 -0500

    Clean data function progress and redefinig of the app

--- app.py
+++ app.py
@@ -1,25 +1,32 @@
+import re
+from pygments import highlight
 import streamlit as st
-from io import StringIO
+from pprint import pprint
 
+# Clean raw text by removing lines, tabs and other unnecessary whitespaces
+def clean_data(text):
+    base_dict = {"book_name": "", "authors": [], "highlights": {}}
+    highlight_dict = {"highlight:": "", "location": "", "added_timestamp": ""}
+    highlights = []
+    # Remove "=" that separates the highlights
+    cleaned_text = [t.splitlines() for t in re.split(r"=+", text)]
 
-def clean_text(text):
-    highlights = [l.splitlines() for l in text]
+    # Remove empty elements in list
+    cleaned_text = [list(filter(None, t)) for t in cleaned_text]
 
-    return [list(filter(None, h)) for h in highlights]
+    return cleaned_text
 
 
 def main():
+
     st.title("Kindle Highlights Manager")
     file = st.file_uploader("Upload Clippings File", type="txt")
     if file is not None:
         raw_text = str(file.read(), "utf-8")
         st.text(raw_text)
+        highlights = clean_data(raw_text)
 
-        raw_text = raw_text.split("==========")
-
-        highlights = clean_text(raw_text)
-
-        print(highlights)
+        pprint(highlights)
 
 
 if __name__ == "__main__":
