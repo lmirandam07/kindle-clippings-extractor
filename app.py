a2156f78c43e7a6628dd61420af409437b86dd45
Luis <luyimiranda07@gmail.com>
2022-03-02 22:03:57 -0500

    Data cleaned and implementation of regex for getting book name and authors

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import re
-from pygments import highlight
 import streamlit as st
 from pprint import pprint
 
 # Clean raw text by removing lines, tabs and other unnecessary whitespaces
 def clean_data(text):
+    # TODO Reframe data structure
     base_dict = {"book_name": "", "authors": [], "highlights": {}}
     highlight_dict = {"highlight:": "", "location": "", "added_timestamp": ""}
     highlights = []
@@ -14,7 +14,17 @@ def clean_data(text):
     # Remove empty elements in list
     cleaned_text = [list(filter(None, t)) for t in cleaned_text]
 
-    return cleaned_text
+    for text in cleaned_text[:5]:
+        if len(text) != 3:
+            continue
+
+        # Regex for getting book name and book authors from first position in text
+        pattern = re.search(r"^(.+) \((.+|.+;.+)\)$", text[0])
+        book_name, book_authors = pattern.groups()
+
+        # TODO Save book content into dict
+
+    return highlights
 
 
 def main():
@@ -22,8 +32,8 @@ def main():
     st.title("Kindle Highlights Manager")
     file = st.file_uploader("Upload Clippings File", type="txt")
     if file is not None:
-        raw_text = str(file.read(), "utf-8")
-        st.text(raw_text)
+        raw_text = str(file.read(), "utf-8").replace("\ufeff", "")
+
         highlights = clean_data(raw_text)
 
         pprint(highlights)
